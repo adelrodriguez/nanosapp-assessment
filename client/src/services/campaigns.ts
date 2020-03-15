c60df492bcdc12dbf03ab28d0487e5b600c1a519
Adel Rodriguez <adelrodriguez@Adels-MacBook-Pro.local>
2020-03-15 19:49:09 -0400

    feat(client): add api configuration and services

---
+++ client/src/services/campaigns.ts
@@ -0,0 +1,8 @@
+import { api } from '../config';
+
+export const campaigns = {
+  summary: () => api.get<CampaignSummary[]>('/campaigns-summary'),
+  list: () => api.get<Campaign[]>('/campaigns'),
+  get: (id: number) => api.get<Campaign>(`/campaigns/${id}`),
+  delete: (id: number) => api.delete(`/campaigns/${id}`)
+};
---
+++ client/src/services/index.ts
@@ -0,0 +1,1 @@
+export { campaigns } from './campaigns';
