0104847c077390099ac46d8396e708091c2f3c1e
Adel Rodriguez <adelrodriguez@Adels-MacBook-Pro.local>
2020-03-15 20:02:50 -0400

    feat(server): Add support for static files

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import logger from 'morgan';
+import path from 'path';
 import createError from 'http-errors';
 import { setupDatabase } from './scripts';
 import { indexRouter, campaignsRouter } from './routes'
@@ -9,6 +10,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Sets up the Lowdb database
 const db = setupDatabase();
